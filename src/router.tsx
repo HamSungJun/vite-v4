f81821aa40f4a27ad313ff6cd4e25156c8aba72b
HamSungJun <tjdwns5123@gmail.com>
2022-12-18 19:25:29 +0900

    페이지 인덱스 파일 이름 변경

--- src/router.tsx
+++ src/router.tsx
@@ -2,10 +2,10 @@ import { lazy } from "react";
 import { createBrowserRouter } from "react-router-dom";
 import Layout from "@app/shared/components/Layout";
 
-const Home = lazy(() => import("@app/pages"));
-const Todo = lazy(() => import("@app/pages/todo"));
-const About = lazy(() => import("@app/pages/about"));
-const Contatct = lazy(() => import("@app/pages/contact"));
+const Home = lazy(() => import("@app/pages/home"));
+const Todo = lazy(() => import("@app/pages/todo/todo"));
+const About = lazy(() => import("@app/pages/about/about"));
+const Contatct = lazy(() => import("@app/pages/contact/contact"));
 const NotFound = lazy(() => import("@app/pages/404"));
 
 export default createBrowserRouter([

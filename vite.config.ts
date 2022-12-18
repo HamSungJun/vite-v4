3ea492f534ae873a37ca57ed2bdb793d4fbe2776
HamSungJun <tjdwns5123@gmail.com>
2022-12-18 19:07:18 +0900

    plugin-legacy

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
 import { defineConfig } from "vite";
 import svgr from "vite-plugin-svgr";
-import react from "@vitejs/plugin-react-swc";
+import legacy from "@vitejs/plugin-legacy";
 import { fileURLToPath, URL } from "url";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svgr(), react()],
+  plugins: [svgr(), legacy()],
   resolve: {
     alias: {
       "@app": fileURLToPath(new URL("./src", import.meta.url)),
